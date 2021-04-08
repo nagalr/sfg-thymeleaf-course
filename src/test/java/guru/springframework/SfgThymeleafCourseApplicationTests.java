dd5c8ae75688f4f9c0d786b86fd5a12390e53843
Ronnen <nagalr@gmail.com>
2021-04-08 16:57:27 +0300

    Convert remote images uri to local project-images

--- src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
+++ src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
@@ -1,13 +1,11 @@
 package guru.springframework;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+//@RunWith(SpringJUnit4ClassRunnerssRunner.class)
+//@SpringJUnit4ClassRunnerApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
 @WebAppConfiguration
 public class SfgThymeleafCourseApplicationTests {
 
