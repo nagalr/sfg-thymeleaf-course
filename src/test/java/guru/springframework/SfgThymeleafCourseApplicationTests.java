679bcd6ed74e128ba30a55867df75d3c3c18a767
Ronnen <nagalr@gmail.com>
2021-04-08 12:40:52 +0300

    Update Spring-Boot parent starter version to avoid conflicts

--- src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
+++ src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
@@ -2,17 +2,16 @@ package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
+//@SpringApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
 @WebAppConfiguration
 public class SfgThymeleafCourseApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
