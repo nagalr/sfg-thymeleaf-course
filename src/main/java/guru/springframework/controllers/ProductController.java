ea08fa3d56798c48dd369cfda2eb973d8789f95c
Ronnen <nagalr@gmail.com>
2021-04-10 15:28:28 +0300

    Add a 'getProductById' method to ProductController to handle @RequestParam (id)

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by jt on 1/20/16.
@@ -9,8 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
+    ProductServiceImpl productService;
+
+    public ProductController(ProductServiceImpl productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/product")
-    public String getProduct(){
+    public String getProduct() {
+        return "product";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@RequestParam Integer id, Model model) {
+        model.addAttribute("prod", productService.getProduct(id));
+
         return "product";
     }
 }
