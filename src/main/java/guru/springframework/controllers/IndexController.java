2b9714f7a7500b296e551c699614140be01f46ec
Ronnen <nagalr@gmail.com>
2021-04-10 18:09:40 +0300

    Add a ProductServiceImpl reference to IndexController. Add 'productsList' an attribute to the index.html view, so a display and iteration over the property values.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    ProductServiceImpl productService;
+
+    public IndexController(ProductServiceImpl productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/")
-    public String getIndex(){
+    public String getIndex(Model model){
+        model.addAttribute("productsList", productService.listProducts());
 
         return "index";
     }
