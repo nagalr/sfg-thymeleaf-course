1ea8433aeb77f79761f7300a0f5e20e5162163d0
Ronnen <nagalr@gmail.com>
2021-04-10 10:23:56 +0300

    fix(Thymeleaf ): Fix a file-reference bug.
    Remove the .html file extension reference from index.html.
    Add few <br> to enable the 'Buy Now!' button view.
    
    Closes #4521

--- src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
+++ src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
@@ -1,13 +1,10 @@
 package guru.springframework;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
+@SpringBootTest
 @WebAppConfiguration
 public class SfgThymeleafCourseApplicationTests {
 
