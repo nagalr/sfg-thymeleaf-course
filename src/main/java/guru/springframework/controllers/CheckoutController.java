3de9cb0d9d7b4c8fe4b5346748c06028ba471abb
Ronnen <nagalr@gmail.com>
2021-04-14 12:14:18 +0300

    Initial commit of the branch

--- src/main/java/guru/springframework/controllers/CheckoutController.java
+++ src/main/java/guru/springframework/controllers/CheckoutController.java
@@ -3,8 +3,8 @@ package guru.springframework.controllers;
 import guru.springframework.comands.CheckoutCommand;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
 
@@ -16,12 +16,14 @@ import javax.validation.Valid;
 public class CheckoutController {
 
     @RequestMapping("/checkout")
-    public String checkoutForm(){
+    public String checkoutForm() {
         return "checkoutform";
     }
 
-    @RequestMapping(value = "/docheckout", method = RequestMethod.POST)
-    public String doCheckout(@Valid CheckoutCommand checkoutCommand, BindingResult bindingResult){
+
+    @PostMapping(value = "/docheckout")
+    public String doCheckout(@Valid CheckoutCommand checkoutCommand,
+                             BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             return "checkoutform";
@@ -30,5 +32,4 @@ public class CheckoutController {
         return "checkoutcomplete";
 
     }
-
 }
